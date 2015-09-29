678777757fcbc8a65e4c66757a0c4ceb5df7a5e6
kochetygovamaria <kochetygova.m@husky.neu.edu>
2015-09-29 17:13:27 -0400

    Prime Assigment on september 29

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -1,14 +1,37 @@
 ﻿namespace FindingPrimeNumbers
 {
-	public class FindingPrimeNumbers
-	{
-		public long FindSumOfPrimeNumbers(int n)
-		{
-			long sum = 0;
+    public class FindingPrimeNumbers
+    {
+        public long FindSumOfPrimeNumbers(int n)
+        {
 
-			// your solution goes in here
 
-			return sum;
-		}
-	}
+            long sum = 0;
+            int count = 0;
+            int num = 2;
+            while (count < n)
+            {
+                if (PrimeNumbers(num))
+                {
+                    count++;
+                    sum = sum + num;
+                }
+
+                num++;
+            }
+
+            return sum;
+        }
+        public bool PrimeNumbers(int num)
+        {
+            bool current = true;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0) current = false;
+
+            }
+
+            return current;
+        }
+    }
 }
