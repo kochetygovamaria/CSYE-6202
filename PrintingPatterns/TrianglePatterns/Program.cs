2a0f0d21348e619e5559d129ccc7d52de5b50376
kochetygovamaria <kochetygova.m@husky.neu.edu>
2015-09-30 22:18:54 -0400

    Print patterns 30 Sep

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -2,36 +2,76 @@
 
 namespace TrianglePatterns
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			DisplayPatternA();
-			DisplayPatternB();
-			DisplayPatternC();
-			DisplayPatternD();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPatternA();
+            DisplayPatternB();
+            DisplayPatternC();
+            DisplayPatternD();
 
-			Console.ReadLine();
-		}
+            Console.ReadLine();
+        }
 
-		static void DisplayPatternA()
-		{
-			// your implementation here
-		}
+        static void DisplayPatternA()
+        {
+            for (int i = 1; i <= 11; i++)
+            {
+                for (int j = 1; j <= 1 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
 
-		static void DisplayPatternB()
-		{
-			// your implementation here
-		}
+        static void DisplayPatternB()
+        {
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
 
-		static void DisplayPatternC()
-		{
-			// your implementation here
-		}
+        static void DisplayPatternC()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (i < j)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
 
-		static void DisplayPatternD()
-		{
-			// your implementation here
-		}
-	}
+        static void DisplayPatternD()
+        {
+
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i > j)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+
+            }
+        }
+    }
 }
+
