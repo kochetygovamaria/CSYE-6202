bdad31555d1b1c5196e8e118163d0f5f399443c6
kochetygovamaria <kochetygova.m@husky.neu.edu>
2015-10-07 21:39:15 -0400

    7

--- GasPump/GasPump/Program.cs
+++ GasPump/GasPump/Program.cs
@@ -2,42 +2,85 @@
 
 namespace GasPump
 {
-	public class Program
-	{
-		public enum GasType
-		{
-			None,
-			RegularGas,
-			MidgradeGas,
-			PremiumGas,
-			DieselFuel				
-		}
-
-		static void Main(string[] args)
-		{
-
-            Console.WriteLine("Please enter the type");
-            String userInput = Console.ReadLine();
-
-            bool check = UserEnteredSentinelValue(userInput);
-            check = UserEnteredValidGasType(userInput);
-            Console.WriteLine("Please entre the amount");
-           userInput = Console.ReadLine();
-
-           /* int amount = 0;
-            Console.WriteLine("");
-            amount = Console.Read();
-            double totalCost = 0;
-            CalculateTotalCost(GasType.RegularGas, amount, totalCost);*/
-
-
+    public class Program
+    {
+        public enum GasType
+        {
+            None,
+            RegularGas,
+            MidgradeGas,
+            PremiumGas,
+            DieselFuel
         }
+
+        static void Main(string[] args)
+        {
+            Boolean q = false;
+
+
+            Console.WriteLine("Please enter the gas type");
+            while (!q)
+            {
+                String gasType = Console.ReadLine();
+                
+                
+                bool check = UserEnteredSentinelValue(gasType);
+                if (check == true)
+                {
+                    q = true;
+                    break;
+                }
+                else
+                {
+
+                    check = UserEnteredValidGasType(gasType);
+                    if (check == false)
+                    {
+                        q = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please entre the amount");
+                    }
+
+                    String userInput = Console.ReadLine();
+                    
+                   
+                    check = UserEnteredValidAmount(userInput);
+                    if (check == false)
+                    {
+                        q = true;
+                        break;
+                    }
+                    else
+                    {
+                        char c = Convert.ToChar(gasType);
+                        GasType i= GasTypeMapper(c);
+
+                        double totalCost = 0;
+                        int z = Convert.ToInt32(userInput);
+                        Console.Write("You purchase  "  +   z   +  i);
+                        Console.ReadLine();
+
+
+
+
+
+                        CalculateTotalCost(i, z, ref totalCost);
+                        Console.Write("You total cost: " +  totalCost);
+                }
+            }
+        }
+    }
+
+   
 
 		// use this method to check and see if sentinel value is entered
 		public static bool UserEnteredSentinelValue(string userInput)
 		{
 			var result = false;
-            if (userInput.ToLower() =="q")
+            if (userInput.ToLower().Equals("q"))
             {
                 result = true;
             }
@@ -50,28 +93,35 @@ namespace GasPump
 		// this does not conform 
 		public static bool UserEnteredValidGasType(string userInput)
 		{
-			var result = false;
-
-            if (userInput.ToLower() == "r" || userInput.ToLower()== "m" || userInput.ToLower()== "p" || userInput.ToLower()=="d")
-                result = true;
-			
-			
+            var result = false;
+            if (userInput.ToLower().Equals("r") || userInput.ToLower().Equals( "m") || userInput.ToLower().Equals("p") || userInput.ToLower().Equals ("d")||userInput.Equals("R") || userInput.Equals("M") || userInput.Equals("P") || userInput.Equals("D"))
+            {
+           
+                
+                    result = true;
+            }
+             
+            
+                
+            
 			return result;
 		}
 
 		// use this method to parse and check the double type entered
 		// please use Double.TryParse() method 
 		public static bool UserEnteredValidAmount(string userInput)
-		{
-            string value;
-            double number;
-			var result = false;
+        {
 
-            value = Double.PositiveInfinity.ToString();
-            if (Double.TryParse(value, out number))
-                Console.WriteLine(number);
+         
+            var result = false;
+            double gasAmount;
+            userInput = Double.MaxValue.ToString();
+            if (Double.TryParse(userInput, out gasAmount))
+                Console.WriteLine(gasAmount);
 
-            result = true;
+            {
+                result=true;
+            }
 
 			
 
@@ -83,18 +133,53 @@ namespace GasPump
 		// this mapping "must" be used within CalculateTotalCost() method later on
 		public static GasType GasTypeMapper(char c)
 		{
-			GasType gasType = GasType.None;
-            
+           
+           
 
-
+            GasType gasType = GasType.None;
+           switch (c)
+            {
+                case 'r':
+                    gasType = GasType.RegularGas;
+                    break;
+                case 'm':
+                    gasType = GasType.MidgradeGas;
+                    break;
+                case 'p':
+                    gasType = GasType.PremiumGas;
+                    break;
+                   
+                case 'd':
+                    gasType = GasType.DieselFuel;
+                    break;
+            }
             return gasType;
 		}
 
 		public static double GasPriceMapper(GasType gasType)
 		{
 			var result = 0.0;
+            
+            if(gasType.Equals(GasType.RegularGas))
 
-			// your implementation here
+            {
+                return 1.94;
+            }
+            if (gasType.Equals(GasType.MidgradeGas))
+            {
+                return 2.00;
+
+
+            }
+            if (gasType.Equals(GasType.PremiumGas))
+            {
+                return 2.24;
+            }
+                    
+            if (gasType.Equals(GasType.DieselFuel))
+            {
+                return 2.94;
+            }
 
 			return result;
 	}
@@ -102,6 +187,7 @@ namespace GasPump
 		public static void CalculateTotalCost(GasType gasType, int gasAmount, ref double totalCost)
 		{
             
+            totalCost = gasAmount * GasPriceMapper(gasType);
                 
             }
 
