6e589739708be663720fc7f525ccadb2fa00aa30
kochetygovamaria <kochetygova.m@husky.neu.edu>
2015-12-11 15:39:21 -0500

    Update Final Project Maria

--- FinalProject/HospitalAdmin.Designer.cs
+++ FinalProject/HospitalAdmin.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,21 +41,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(487, 336);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(269, 80);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Price";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // HospitalAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1286, 677);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "HospitalAdmin";
             this.Text = "Form3";
@@ -68,6 +57,5 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
